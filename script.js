e31fafaafe81db78901376bef65188d9dd1b9692
Archita Gupta <archita2705gupta@gmail.com>
2025-12-14 17:34:11 +0530

    updated hamburger

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-let hamburger = document.getElementsByClassName("lines")[0];
+// let hamburger = document.getElementsByClassName("lines")[0];
+// let menu = document.querySelector(".menu");
+
+// hamburger.addEventListener("click", () => {
+//     menu.classList.toggle("active");
+// });
+// let back=document.querySelector(".back");
+// console.log(back);
+// back.addEventListener("click", () => {
+//     menu.classList.toggle("deactive");
+// });
+
+let hamburger = document.querySelector(".lines");
 let menu = document.querySelector(".menu");
+let back = document.querySelector(".back");
 
 hamburger.addEventListener("click", () => {
     menu.classList.toggle("active");
 });
+
+back.addEventListener("click", () => {
+    menu.classList.toggle("active");
+});
+
+
